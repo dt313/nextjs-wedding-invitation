7f3b6c3ab9f0bd535ae6511ff0b5bf068479a5f0
dt313 <dt313.dev21@gmail.com>
2023-12-13 15:22:55 +0900

    add meta-daat

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -8,8 +8,12 @@ const cx = classNames.bind(styles);
 export async function generateMetadata({ params, searchParams }) {
   const name = decodeURIComponent(params.name) || "You";
   return {
-    title: `Thiệp cưới - ${name}`,
-    description: "Helllo",
+    title: `Thiệp cưới của Linh và Đức | Mời ${name}`,
+    openGraph: {
+      images: [
+        "https://hi.possiblewedding.com/wp-content/uploads/2022/05/cody-black-VDb0wxbfG6k-unsplash_compressed-1.jpg",
+      ],
+    },
   };
 }
 function Main({ params }) {
